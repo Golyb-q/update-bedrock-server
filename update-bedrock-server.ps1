0febdea3881a3fea57183b568e5ef573d2cde23f
G0lyb-q <zavy58p@gmail.com>
2023-11-22 20:08:47 +0300

    translated to en

--- update-bedrock-server.ps1
+++ update-bedrock-server.ps1
@@ -1,13 +1,13 @@
 $sourcePath = 'bedrock-server'
 $destinationPath = 'bedrock-server-backup\{0}' -f (Get-Date -Format "yyyyMMdd_HHmmss")
 New-Item -ItemType Directory -Path $destinationPath
-Write-Output "Cздание бекапа"
+Write-Output "Creating a backup"
 Copy-Item -Path "$sourcePath\worlds" -Destination $destinationPath -Recurse
 Copy-Item -Path "$sourcePath\allowlist.json" -Destination $destinationPath
 Copy-Item -Path "$sourcePath\permissions.json" -Destination $destinationPath
 Copy-Item -Path "$sourcePath\server.properties" -Destination $destinationPath
 
-Write-Output "Cздание бекапа завершено, удаление старой версии"
+Write-Output "Backup creation completed, old version deleted"
 Remove-Item -Path $sourcePath -Recurse -Force
 
 C:\CMD\curl-8.4.0_6-win64-mingw\bin\curl.exe --user-agent "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36" https://www.minecraft.net/ru-ru/download/server/bedrock > version.txt
@@ -18,11 +18,11 @@ foreach ($match in $matches) {
     Write-Output $match.Value > version.txt
 }
 $versionFile = Get-Content -Path "version.txt" -First 1
-Write-Output "Актуальная ссылка получена. Идет скачивание"
+Write-Output "The current link has been received. Downloading..."
 Invoke-WebRequest -Uri $versionFile -OutFile "bedrock-server.zip"
-Write-Output "Получен архив, идет рспаковка"
-Expand-Archive -Path 'bedrock-server.zip' -DestinationPath 'bedrock-server'
+Write-Output "The archive has been received, unpacking..."
+Expand-Archive -Path 'bedrock-server.zip' -DestinationPath $sourcePath
 Remove-Item -Path 'bedrock-server.zip' -Force
 
-Write-Output "Перенос бекапа на новую версию"
+Write-Output "Transferring a backup to a new version"
 Copy-Item -Path $destinationPath\* -Destination $sourcePath -Recurse
